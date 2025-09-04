25da2cff5d642aa02832a5403f5a7d187e625dc7
Benny L.E.P <bendo01@gmail.com>
2025-09-04 21:26:15 +0800

    fix

---
+++ src/models/feeder/master/komponen_evaluasi_kelas/_entities/komponen_evaluasi_kelas.rs
@@ -0,0 +1,27 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
+#[sea_orm(schema_name = "feeder_master", table_name = "komponen_evaluasi_kelas")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub id: Uuid,
+    pub id_komponen_evaluasi: Uuid,
+    pub id_kelas_kuliah: Uuid,
+    pub id_jenis_evaluasi: Uuid,
+    pub nama: Option<String>,
+    pub nama_inggris: Option<String>,
+    pub nomor_urut: i32,
+    pub bobot_evaluasi: String,
+    pub last_update: Date,
+    pub tgl_create: Date,
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
+    pub sync_at: Option<DateTime>,
+    pub deleted_at: Option<DateTime>,
+    pub created_by: Option<Uuid>,
+    pub updated_by: Option<Uuid>,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
---
+++ src/models/feeder/master/komponen_evaluasi_kelas/_entities/mod.rs
@@ -0,0 +1,2 @@
+pub mod prelude;
+pub mod komponen_evaluasi_kelas;
