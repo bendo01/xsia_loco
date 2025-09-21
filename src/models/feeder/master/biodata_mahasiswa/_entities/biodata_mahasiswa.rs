8ebb9e839984cc8dc287dee6e552d8c47cbf33b9
Benny L.E.P <bendo01@gmail.com>
2025-09-21 18:58:42 +0800

    fix

--- src/models/feeder/master/biodata_mahasiswa/_entities/biodata_mahasiswa.rs
+++ src/models/feeder/master/biodata_mahasiswa/_entities/biodata_mahasiswa.rs
@@ -1,5 +1,6 @@
 use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
+use crate::library::deserialization::{de_opt_date_dmy, de_opt_i32};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
 #[sea_orm(schema_name = "feeder_master", table_name = "biodata_mahasiswa")]
@@ -9,8 +10,10 @@ pub struct Model {
     pub nama_mahasiswa: Option<String>,
     pub jenis_kelamin: Option<String>,
     pub tempat_lahir: Option<String>,
-    pub tanggal_lahir: Option<Date>,
-    pub id_mahasiswa: Option<Uuid>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tanggal_lahir: Option<chrono::NaiveDate>,
+    pub id_mahasiswa: Option<uuid::Uuid>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub id_agama: Option<i32>,
     pub nama_agama: Option<String>,
     pub nik: Option<String>,
@@ -20,7 +23,9 @@ pub struct Model {
     pub kewarganegaraan: Option<String>,
     pub jalan: Option<String>,
     pub dusun: Option<String>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub rt: Option<i32>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub rw: Option<i32>,
     pub kelurahan: Option<String>,
     pub kode_pos: Option<String>,
@@ -33,38 +38,51 @@ pub struct Model {
     pub telepon: Option<String>,
     pub handphone: Option<String>,
     pub email: Option<String>,
+    #[serde(deserialize_with = "de_opt_boolish")]
     pub penerima_kps: Option<bool>,
     pub nomor_kps: Option<String>,
     pub nik_ayah: Option<String>,
     pub nama_ayah: Option<String>,
-    pub tanggal_lahir_ayah: Option<Date>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tanggal_lahir_ayah: Option<chrono::NaiveDate>,
     pub id_pendidikan_ayah: Option<String>,
     pub nama_pendidikan_ayah: Option<String>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub id_pekerjaan_ayah: Option<i32>,
     pub nama_pekerjaan_ayah: Option<String>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub id_penghasilan_ayah: Option<i32>,
     pub nama_penghasilan_ayah: Option<String>,
     pub nik_ibu: Option<String>,
     pub nama_ibu_kandung: Option<String>,
-    pub tanggal_lahir_ibu: Option<Date>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tanggal_lahir_ibu: Option<chrono::NaiveDate>,
     pub id_pendidikan_ibu: Option<String>,
     pub nama_pendidikan_ibu: Option<String>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub id_pekerjaan_ibu: Option<i32>,
     pub nama_pekerjaan_ibu: Option<String>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub id_penghasilan_ibu: Option<i32>,
     pub nama_penghasilan_ibu: Option<String>,
     pub nama_wali: Option<String>,
-    pub tanggal_lahir_wali: Option<Date>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tanggal_lahir_wali: Option<chrono::NaiveDate>,
     pub id_pendidikan_wali: Option<String>,
     pub nama_pendidikan_wali: Option<String>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub id_pekerjaan_wali: Option<i32>,
     pub nama_pekerjaan_wali: Option<String>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub id_penghasilan_wali: Option<i32>,
     pub nama_penghasilan_wali: Option<String>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub id_kebutuhan_khusus_mahasiswa: Option<i32>,
     pub nama_kebutuhan_khusus_mahasiswa: Option<String>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub id_kebutuhan_khusus_ayah: Option<i32>,
     pub nama_kebutuhan_khusus_ayah: Option<String>,
+    #[serde(deserialize_with = "de_opt_i32")]
     pub id_kebutuhan_khusus_ibu: Option<i32>,
     pub nama_kebutuhan_khusus_ibu: Option<String>,
     pub status_sync: Option<String>,
