a09253ef7f6c9631d27aae730127a8b2d502a8d9
Benny L.E.P <bendo01@gmail.com>
2025-10-19 14:56:26 +0800

    fix

--- src/workers/feeder_dikti/downstream/master/mod.rs
+++ src/workers/feeder_dikti/downstream/master/mod.rs
@@ -4,3 +4,4 @@ pub mod get_biodata_mahasiswa;
 pub mod get_list_riwayat_pendidikan_mahasiswa;
 pub mod get_nilai_transfer_pendidikan_mahasiswa;
 pub mod get_riwayat_nilai_mahasiswa;
+pub mod get_krs_mahasiswa;
