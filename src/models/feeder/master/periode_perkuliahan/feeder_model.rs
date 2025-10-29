4a7cf7379a3d8a9e016b4b79c755823e9f2466a9
Benny L.E.P <bendo01@gmail.com>
2025-10-29 23:30:38 +0800

    fix

--- src/models/feeder/master/periode_perkuliahan/feeder_model.rs
+++ src/models/feeder/master/periode_perkuliahan/feeder_model.rs
@@ -3,8 +3,60 @@ use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+/// Model for GetListPeriodePerkuliahan API response
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ModelInputListPeriodePerkuliahan {}
+pub struct ModelInputListPeriodePerkuliahan {
+    pub id_prodi: Uuid,
+    pub nama_program_studi: String,
+    pub id_semester: String,
+    pub nama_semester: String,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub jumlah_target_mahasiswa_baru: Option<i32>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tanggal_awal_perkuliahan: Option<NaiveDate>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tanggal_akhir_perkuliahan: Option<NaiveDate>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub calon_ikut_seleksi: Option<i32>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub calon_lulus_seleksi: Option<i32>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub daftar_sbg_mhs: Option<i32>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub pst_undur_diri: Option<i32>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub jml_mgu_kul: Option<i32>,
+    pub metode_kul: Option<String>,
+    pub metode_kul_eks: Option<String>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tgl_create: Option<NaiveDate>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub last_update: Option<NaiveDate>,
+    pub status_sync: String,
+}
 
+/// Model for GetDetailPeriodePerkuliahan API response
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ModelInputDetailPeriodePerkuliahan {}
+pub struct ModelInputDetailPeriodePerkuliahan {
+    pub id_prodi: Uuid,
+    pub nama_program_studi: String,
+    pub id_semester: String,
+    pub nama_semester: String,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub jumlah_target_mahasiswa_baru: Option<i32>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub jumlah_pendaftar_ikut_seleksi: Option<i32>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub jumlah_pendaftar_lulus_seleksi: Option<i32>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub jumlah_daftar_ulang: Option<i32>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub jumlah_mengundurkan_diri: Option<i32>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tanggal_awal_perkuliahan: Option<NaiveDate>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tanggal_akhir_perkuliahan: Option<NaiveDate>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub jumlah_minggu_pertemuan: Option<i32>,
+    pub status_sync: String,
+}
