dd4482b0790670b4226f9ff4fc98500338d0a1df
Benny L.E.P <bendo01@gmail.com>
2025-10-30 19:43:15 +0800

    fix

--- src/models/feeder/master/kelas_kuliah/_entities/kelas_kuliah.rs
+++ src/models/feeder/master/kelas_kuliah/_entities/kelas_kuliah.rs
@@ -7,20 +7,28 @@ use chrono::NaiveDate;
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub id: Uuid,
+    pub sync_at: Option<DateTime>,
+    pub created_by: Option<Uuid>,
+    pub updated_by: Option<Uuid>,
+    pub created_at: Option<DateTime>,
+    pub updated_at: Option<DateTime>,
+    pub deleted_at: Option<DateTime>,
+    
+    #[sea_orm(unique)]
     pub id_kelas_kuliah: Uuid,
-    pub id_prodi: Uuid,
-    pub nama_program_studi: String,
-    pub id_semester: String,
-    pub nama_semester: String,
-    pub id_matkul: Uuid,
-    pub kode_mata_kuliah: String,
-    pub nama_mata_kuliah: String,
-    pub sks_mk: f32,
-    pub sks_tm: f32,
-    pub sks_prak: f32,
-    pub sks_prak_lap: f32,
-    pub sks_sim: f32,
-    pub nama_kelas_kuliah: String,
+    pub id_prodi: Option<Uuid>,
+    pub nama_program_studi: Option<String>,
+    pub id_semester: Option<String>,
+    pub nama_semester: Option<String>,
+    pub id_matkul: Option<Uuid>,
+    pub kode_mata_kuliah: Option<String>,
+    pub nama_mata_kuliah: Option<String>,
+    pub nama_kelas_kuliah: Option<String>,
+    pub sks_mk: Option<f32>,
+    pub sks_tm: Option<f32>,
+    pub sks_prak: Option<f32>,
+    pub sks_prak_lap: Option<f32>,
+    pub sks_sim: Option<f32>,
     pub bahasan: Option<String>,
     pub tanggal_mulai_efektif: Option<NaiveDate>,
     pub tanggal_akhir_efektif: Option<NaiveDate>,
@@ -28,13 +36,14 @@ pub struct Model {
     pub tanggal_tutup_daftar: Option<NaiveDate>,
     pub prodi_penyelenggara: Option<String>,
     pub perguruan_tinggi_penyelenggara: Option<String>,
-    pub sync_at: Option<DateTime>,
-    pub created_by: Option<Uuid>,
-    pub updated_by: Option<Uuid>,
-    pub created_at: Option<DateTime>,
-    pub updated_at: Option<DateTime>,
-    pub deleted_at: Option<DateTime>,
+    pub sks: Option<f32>,
+    pub id_dosen: Option<String>,
+    pub nama_dosen: Option<String>,
+    pub jumlah_mahasiswa: Option<i32>,
+    pub apa_untuk_pditt: Option<bool>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
 pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
--- src/models/feeder/master/kelas_kuliah/feeder_model.rs
+++ src/models/feeder/master/kelas_kuliah/feeder_model.rs
@@ -1,59 +1,59 @@
-use crate::library::deserialization::{de_f32, de_opt_date_dmy, de_opt_i32};
+use crate::library::deserialization::{de_f32, de_opt_date_dmy, de_opt_i32, de_opt_bool};
 use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct GetDetailKelasKuliahResponse {
-    #[serde(rename = "error_code")]
-    pub error_code: i32,
-    #[serde(rename = "error_desc")]
-    pub error_desc: String,
-    pub data: Vec<KelasKuliah>,
+pub struct ModelInputDetailKelasKuliah {
+    pub id_kelas_kuliah: Uuid,
+    pub id_prodi: Option<Uuid>,
+    pub nama_program_studi: Option<String>,
+    pub id_semester: Option<String>,
+    pub nama_semester: Option<String>,
+    pub id_matkul: Option<Uuid>,
+    pub kode_mata_kuliah: Option<String>,
+    pub nama_mata_kuliah: Option<String>,
+    #[serde(deserialize_with = "de_f32")]
+    pub sks_mk: Option<f32>,
+    #[serde(deserialize_with = "de_f32")]
+    pub sks_tm: Option<f32>,
+    #[serde(deserialize_with = "de_f32")]
+    pub sks_prak: Option<f32>,
+    #[serde(deserialize_with = "de_f32")]
+    pub sks_prak_lap: Option<f32>,
+    #[serde(deserialize_with = "de_f32")]
+    pub sks_sim: Option<f32>,
+    pub nama_kelas_kuliah: Option<String>,
+    pub bahasan: Option<String>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tanggal_mulai_efektif: Option<NaiveDate>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tanggal_akhir_efektif: Option<NaiveDate>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub kapasitas: Option<i32>,
+    #[serde(deserialize_with = "de_opt_date_dmy")]
+    pub tanggal_tutup_daftar: Option<NaiveDate>,
+    pub prodi_penyelenggara: Option<String>,
+    pub perguruan_tinggi_penyelenggara: Option<String>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct KelasKuliah {
-    #[serde(rename = "id_kelas_kuliah")]
+pub struct ModelInputListKelasKuliah {
     pub id_kelas_kuliah: Uuid,
-    #[serde(rename = "id_prodi")]
-    pub id_prodi: Uuid,
-    #[serde(rename = "nama_program_studi")]
-    pub nama_program_studi: String,
-    #[serde(rename = "id_semester")]
-    pub id_semester: String,
-    #[serde(rename = "nama_semester")]
-    pub nama_semester: String,
-    #[serde(rename = "id_matkul")]
-    pub id_matkul: Uuid,
-    #[serde(rename = "kode_mata_kuliah")]
-    pub kode_mata_kuliah: String,
-    #[serde(rename = "nama_mata_kuliah")]
-    pub nama_mata_kuliah: String,
-    #[serde(rename = "sks_mk", deserialize_with = "de_f32")]
-    pub sks_mk: f32,
-    #[serde(rename = "sks_tm", deserialize_with = "de_f32")]
-    pub sks_tm: f32,
-    #[serde(rename = "sks_prak", deserialize_with = "de_f32")]
-    pub sks_prak: f32,
-    #[serde(rename = "sks_prak_lap", deserialize_with = "de_f32")]
-    pub sks_prak_lap: f32,
-    #[serde(rename = "sks_sim", deserialize_with = "de_f32")]
-    pub sks_sim: f32,
-    #[serde(rename = "nama_kelas_kuliah")]
-    pub nama_kelas_kuliah: String,
-    #[serde(rename = "bahasan")]
-    pub bahasan: Option<String>,
-    #[serde(rename = "tanggal_mulai_efektif", deserialize_with = "de_opt_date_dmy")]
-    pub tanggal_mulai_efektif: Option<NaiveDate>,
-    #[serde(rename = "tanggal_akhir_efektif", deserialize_with = "de_opt_date_dmy")]
-    pub tanggal_akhir_efektif: Option<NaiveDate>,
-    #[serde(rename = "kapasitas", deserialize_with = "de_opt_i32")]
-    pub kapasitas: Option<i32>,
-    #[serde(rename = "tanggal_tutup_daftar", deserialize_with = "de_opt_date_dmy")]
-    pub tanggal_tutup_daftar: Option<NaiveDate>,
-    #[serde(rename = "prodi_penyelenggara")]
-    pub prodi_penyelenggara: Option<String>,
-    #[serde(rename = "perguruan_tinggi_penyelenggara")]
-    pub perguruan_tinggi_penyelenggara: Option<String>,
+    pub id_prodi: Option<Uuid>,
+    pub nama_program_studi: Option<String>,
+    pub id_semester: Option<String>,
+    pub nama_semester: Option<String>,
+    pub id_matkul: Option<Uuid>,
+    pub kode_mata_kuliah: Option<String>,
+    pub nama_mata_kuliah: Option<String>,
+    pub nama_kelas_kuliah: Option<String>,
+    #[serde(deserialize_with = "de_f32")]
+    pub sks: Option<f32>,
+    pub id_dosen: Option<String>,
+    pub nama_dosen: Option<String>,
+    #[serde(deserialize_with = "de_opt_i32")]
+    pub jumlah_mahasiswa: Option<i32>,
+    #[serde(deserialize_with = "de_opt_bool")]
+    pub apa_untuk_pditt: Option<i32>,
 }
