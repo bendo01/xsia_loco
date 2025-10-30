5886281f0687cd2a89244a83fd6035da69ad95c3
Benny L.E.P <bendo01@gmail.com>
2025-10-30 22:25:37 +0800

    fix

--- src/models/feeder/master/kurikulum/_entities/kurikulum.rs
+++ src/models/feeder/master/kurikulum/_entities/kurikulum.rs
@@ -6,12 +6,20 @@ use serde::{Deserialize, Serialize};
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub id: Uuid,
+    pub created_at: Option<DateTime>,
+    pub updated_at: Option<DateTime>,
+    pub deleted_at: Option<DateTime>,
+    pub sync_at: Option<DateTime>,
+    pub created_by: Option<Uuid>,
+    pub updated_by: Option<Uuid>,
+    
+    // Fields from feeder API
     pub id_kurikulum: Option<Uuid>,
-    pub jml_sem_normal: Option<i32>,
-    pub id_jenj_didik: Option<String>,
     pub nama_kurikulum: Option<String>,
     pub id_prodi: Option<Uuid>,
     pub nama_program_studi: Option<String>,
+    pub id_jenj_didik: Option<i32>,
+    pub jml_sem_normal: Option<i32>,
     pub id_semester: Option<String>,
     pub semester_mulai_berlaku: Option<String>,
     pub jumlah_sks_lulus: Option<f32>,
@@ -20,12 +28,6 @@ pub struct Model {
     pub jumlah_sks_mata_kuliah_wajib: Option<f32>,
     pub jumlah_sks_mata_kuliah_pilihan: Option<f32>,
     pub status_sync: Option<String>,
-    pub sync_at: Option<DateTime>,
-    pub created_by: Option<Uuid>,
-    pub updated_by: Option<Uuid>,
-    pub created_at: Option<DateTime>,
-    pub updated_at: Option<DateTime>,
-    pub deleted_at: Option<DateTime>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
